d2c42f9a9babfd5c752a303a94256cfa81ada2dc
Ewiiz <128278275+Ewiiz@users.noreply.github.com>
2024-03-14 22:40:58 +0400

    PRODUCTS : Migration + Model

---
+++ database/migrations/1710441432111_create_products_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'products'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title').notNullable()
+      table.text('details').nullable()
+      table.decimal('price', 8,2).notNullable()
+      table.string('cover_image').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
