5a05de5d4881d23d8301aca6483cb9492efd156c
Ewiiz <128278275+Ewiiz@users.noreply.github.com>
2024-03-18 08:21:42 +0400

    MIDDLEWARE : ADMIN

---
+++ app/middleware/admin_middleware.ts
@@ -0,0 +1,28 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import type { NextFn } from '@adonisjs/core/types/http'
+
+export default class AdminMiddleware {
+  async handle(ctx: HttpContext, next: NextFn) {
+    /**
+     * Middleware logic goes here (before the next call)
+     */
+    const user = ctx.auth.use('web').user
+
+    if (!user) {
+      return ctx.response.unauthorized({
+        message: 'Vous devez être connecté pour accéder à cette ressource.',
+      })
+    }
+    // Vérifier si l'utilisateur est un administrateur
+    if (!user.isAdmin) {
+      const data = { firstname: user.firstname, admin: user.isAdmin }
+      return ctx.response.forbidden({
+        data,
+        message: "Vous n'avez pas les autorisations suffisantes pour accéder à cette ressource.",
+      })
+    }
+
+    // Si l'utilisateur est connecté et est un administrateur, passer à l'étape suivante
+    await next()
+  }
+}
